ffa743519049141aa2e119fb53714342484ed1a7
Tejas Patil <tejas.coder@gmail.com>
2021-09-08 12:08:33 +0530

    Updated designscript.js

--- designscript.js
+++ designscript.js
@@ -1,6 +1,5 @@
-const $ = s => document.querySelectorAll(s)
+const $ = s => document.querySelectorAll(s);
 const on = (ev, el, cb) =>
-  el.addEventListener(ev, e => cb(e, el))
-$('a').forEach(el =>
-  on('click', el, e => e.preventDefault())
-)
+  el.addEventListener(ev, e => cb(e, el));
+$('a').forEach((el) =>
+  on('click', el, e => e.preventDefault()));
